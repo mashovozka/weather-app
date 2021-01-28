463ce50ecca37e0e469758609180a69f2b65f21c
Mariia Tkachova <mtkachova@oakfusion.pl>
2021-01-28 23:04:12 +0100

    removed google api

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ function App() {
                   <OtherLocations/>
                 </div>
                 <div className='MapsWrapper'>
-                  <GoogleMaps
-                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCSCREDVjbJ-WOVvD2JTWGCViXbwgjbyP0`}
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `400px` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                  />
-                </div>
+                {/*  <GoogleMaps*/}
+                {/*    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCSCREDVjbJ-WOVvD2JTWGCViXbwgjbyP0`}*/}
+                {/*    loadingElement={<div style={{ height: `100%` }} />}*/}
+                {/*    containerElement={<div style={{ height: `400px` }} />}*/}
+                {/*    mapElement={<div style={{ height: `100%` }} />}*/}
+                {/*  />*/}
+                {/*</div>*/}
               </div>
           </div>
          </ContainerStickyAlert>
