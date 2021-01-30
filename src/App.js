769fd3349c45017595f287cf97e7c512c327706e
Mariia Tkachova <mtkachova@oakfusion.pl>
2021-01-30 18:15:35 +0100

    add google maps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import OtherLocations from './components/OtherLocations/OtherLocations';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch';
 import ContainerStickyAlert from './components/StickyAlert/StickyAlert';
 import { SnackbarProvider } from 'notistack';
+import GoogleMaps from "./components/GoogleMaps/GoogleMaps";
 
 function App() {
   return (
@@ -20,6 +21,12 @@ function App() {
                   <OtherLocations/>
                 </div>
                 <div className='MapsWrapper'>
+                    <GoogleMaps
+                      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCSCREDVjbJ-WOVvD2JTWGCViXbwgjbyP0`}
+                      loadingElement={<div style={{ height: `100%` }} />}
+                      containerElement={<div style={{ height: `400px` }} />}
+                      mapElement={<div style={{ height: `100%` }} />}
+                    />
                </div>
               </div>
           </div>
---
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -0,0 +1,62 @@
+import React, {useRef, useState} from 'react';
+import {GoogleMap, InfoWindow, Marker, withGoogleMap, withScriptjs} from "react-google-maps";
+import {useSelector} from "react-redux";
+import classes from './GoogleMaps.module.sass';
+
+function Map() {
+  const mapRef = useRef();
+  const currentLocation = useSelector(state => state.currentLocationWeather)
+  const locations = useSelector(state => state.otherLocations);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  return currentLocation && (
+    <GoogleMap
+      defaultZoom={1.5}
+      defaultCenter={{lat: currentLocation.coord.lat, lng: currentLocation.coord.lon}}
+      ref={mapRef}
+    >
+      <Marker
+        position={{lat: currentLocation.coord.lat, lng: currentLocation.coord.lon}}
+        onClick={() => {
+          setSelectedLocation(currentLocation)
+        }}
+        icon={{
+          url: `https://openweathermap.org/img/w/${currentLocation.weather[0].icon}.png`,
+        }}
+      />
+      {locations.map(location => (
+        <Marker
+          key={location.id}
+          position={{lat: location.coord.lat, lng: location.coord.lon}}
+          icon={{
+            url: `https://openweathermap.org/img/w/${location.weather[0].icon}.png`,
+          }}
+          onClick={() => {
+            setSelectedLocation(location)
+          }}
+
+        >
+        </Marker>
+      ))}
+      {selectedLocation &&
+      <InfoWindow
+        onCloseClick={() => {
+          setSelectedLocation(null);
+        }}
+        position={{lat: selectedLocation.coord.lat, lng: selectedLocation.coord.lon}}
+      >
+        <div>
+          <div className={classes.cityName}>{selectedLocation.name}, {selectedLocation.sys.country}</div>
+          <div>{Math.round(selectedLocation.main.temp)}°C</div>
+          <div>feels like: {Math.round(selectedLocation.main.feels_like)}</div>
+        </div>
+      </InfoWindow>
+      }
+    </GoogleMap>
+  )
+}
+
+const GoogleMaps = withScriptjs(withGoogleMap(Map));
+
+export default GoogleMaps;
+
